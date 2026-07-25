fa3614e6b55defd15a4ed73c6237038e80f01319
SURENDHAR K <145741260+SURENDHAR-725@users.noreply.github.com>
2026-07-25 11:08:12 +0530

    functionality to reverse a string

---
+++ Reverse a string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String arr[]=s.split("");
+		
+		for(int i=arr.length-1;i>=0;i--){
+		    System.out.print(arr[i]);
+		    
+		}
+
+	}
+}
+
+
+====================================
+
+Sample Input
+surendhar
+Your Output
+rahdnerus
