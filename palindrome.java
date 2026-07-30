b98c566810dd7f454c61f93a49e6a7199840e147
SURENDHAR K <145741260+SURENDHAR-725@users.noreply.github.com>
2026-07-30 19:14:52 +0530

    Refactor palindrome check to use two-pointer method
    Refactored palindrome checking logic to use two-pointer technique for improved efficiency.

--- palindrome.java
+++ palindrome.java
@@ -34,3 +34,65 @@ Sample Input
 madam
 Your Output
 Palindrome
+===========================================
+
+
+	import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+	    String rev="";
+	    for(int i=s.length()-1;i>=0;i--){
+	        rev+=s.charAt(i);
+	    }
+	    
+	    if(s.equals(rev)){
+	        System.out.print("palindrome");
+	    }
+	    else{
+	        System.out.print("Not Plaindrome");
+	    }
+
+	}
+}
+===================================================
+
+	import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+	    int right=s.length()-1;
+	    int left=0;
+	    
+	    boolean palindrome=true;
+	    while(left<right){
+	        if(s.charAt(left)!=s.charAt(right)){
+	            palindrome=false;
+	            
+	        }
+	        left++;
+	        right--;
+	        
+	    }
+	    
+	    if(palindrome){
+	        System.out.print("palindrome");
+	    }
+	    else{
+	        System.out.print("Not a palindrome");
+	    }
+	}
+}
+
