45fec00b82820025cafc3b0d1a110bfd6711df69
SURENDHAR K <145741260+SURENDHAR-725@users.noreply.github.com>
2026-07-31 18:38:15 +0530

    Implement space removal from input string
    This program reads a string input, removes spaces, and prints the modified string.

---
+++ Remove spaces.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine().toLowerCase();
+		s=s.replaceAll(" ","");
+		System.out.print(s);
+
+	}
+}
+
+==========================================
+Sample Input
+prog ramm in g
+Your Output
+programming
+========================================
